4eff8667be1f225bb1a2529d38bc2fda88cf83d7
Anton Maminov <anton.linux@gmail.com>
2025-08-01 13:16:13 +0300

    remove duplicated lines

--- spec/response_spec.cr
+++ spec/response_spec.cr
@@ -17,7 +17,6 @@ describe "Response" do
     it "should return true if bot detected" { response.bot?.should be_true }
     it "should return bot name" do
       response.bot.name.should eq("Googlebot")
-      response.bot_name.should eq("Googlebot")
     end
   end
 
@@ -28,7 +27,6 @@ describe "Response" do
     it "should return true if bot detected" { response.bot?.should be_true }
     it "should return bot name" do
       response.bot.name.should eq("Googlebot")
-      response.bot_name.should eq("Googlebot")
     end
   end
 
@@ -39,7 +37,6 @@ describe "Response" do
     it "should return true if engine detected" { response.browser_engine?.should be_true }
     it "should return engine name" do
       response.browser_engine.name.should eq "Edge"
-      response.browser_engine_name.should eq "Edge"
     end
   end
 
@@ -50,11 +47,9 @@ describe "Response" do
     it "should return true if browser detected" { response.browser?.should be_true }
     it "should return browser name" do
       response.browser.name.should eq "Microsoft Edge"
-      response.browser_name.should eq "Microsoft Edge"
     end
     it "should return browser version" do
       response.browser.version.should eq "12.0"
-      response.browser_version.should eq "12.0"
     end
   end
 
@@ -65,11 +60,9 @@ describe "Response" do
     it "should return true if camera detected" { response.camera?.should be_true }
     it "should return camera vendor" do
       response.camera.vendor.should eq "Nikon"
-      response.camera_vendor.should eq "Nikon"
     end
     it "should return camera model" do
       response.camera.device.should eq "Coolpix S800c"
-      response.camera_model.should eq "Coolpix S800c"
     end
   end
 
@@ -80,11 +73,9 @@ describe "Response" do
     it "should return true if car browser detected" { response.car_browser?.should be_true }
     it "should return car browser vendor" do
       response.car_browser.vendor.should eq "Tesla"
-      response.car_browser_vendor.should eq "Tesla"
     end
     it "should return car browser model" do
       response.car_browser.model.should eq "Model S"
-      response.car_browser_model.should eq "Model S"
     end
   end
 
@@ -95,11 +86,9 @@ describe "Response" do
     it "should return true if console detected" { response.console?.should be_true }
     it "should return console vendor" do
       response.console.vendor.should eq "Sony"
-      response.console_vendor.should eq "Sony"
     end
     it "should return console model" do
       response.console.model.should eq "PlayStation 3"
-      response.console_model.should eq "PlayStation 3"
     end
   end
 
@@ -110,11 +99,9 @@ describe "Response" do
     it "should return true if feedreader detected" { response.feed_reader?.should be_true }
     it "should return name" do
       response.feed_reader.name.should eq "Feeddler RSS Reader"
-      response.feed_reader_name.should eq "Feeddler RSS Reader"
     end
     it "should return version" do
       response.feed_reader.version.should eq "2.4"
-      response.feed_reader_version.should eq "2.4"
     end
   end
 
@@ -125,11 +112,9 @@ describe "Response" do
     it "should return true if library detected" { response.library?.should be_true }
     it "should return name" do
       response.library.name.should eq "curl"
-      response.library_name.should eq "curl"
     end
     it "should return version" do
       response.library.version.should eq "7.21.0"
-      response.library_version.should eq "7.21.0"
     end
   end
 
@@ -140,11 +125,9 @@ describe "Response" do
     it "should return true if mediaplayer detected" { response.mediaplayer?.should be_true }
     it "should return name" do
       response.mediaplayer.name.should eq "iTunes"
-      response.mediaplayer_name.should eq "iTunes"
     end
     it "should return version" do
       response.mediaplayer.version.should eq "10.2.1"
-      response.mediaplayer_version.should eq "10.2.1"
     end
   end
 
@@ -155,11 +138,9 @@ describe "Response" do
     it "should return true if mobile app detected" { response.mobile_app?.should be_true }
     it "should return name" do
       response.mobile_app.name.should eq "WhatsApp"
-      response.mobile_app_name.should eq "WhatsApp"
     end
     it "should return version" do
       response.mobile_app.version.should eq "2.6.4"
-      response.mobile_app_version.should eq "2.6.4"
     end
   end
 
@@ -170,15 +151,12 @@ describe "Response" do
     it "should return true if mobile device detected" { response.mobile_device?.should be_true }
     it "should return vendor" do
       response.mobile_device.vendor.should eq "Blackview"
-      response.mobile_device_vendor.should eq "Blackview"
     end
     it "should return type" do
       response.mobile_device.type.should eq "smartphone"
-      response.mobile_device_type.should eq "smartphone"
     end
     it "should return model" do
       response.mobile_device.model.should eq "BV6000"
-      response.mobile_device_model.should eq "BV6000"
     end
   end
 
@@ -189,11 +167,9 @@ describe "Response" do
     it "should return true if OS detected" { response.os?.should be_true }
     it "should return name" do
       response.os.name.should eq "Fedora"
-      response.os_name.should eq "Fedora"
     end
     it "should return version" do
       response.os.version.should eq "1.9.0.8"
-      response.os_version.should eq "1.9.0.8"
     end
   end
 
@@ -203,11 +179,9 @@ describe "Response" do
     it "should return true if PIM detected" { response.pim?.should be_true }
     it "should return name" do
       response.pim.name.should eq "Microsoft Outlook"
-      response.pim_name.should eq "Microsoft Outlook"
     end
     it "should return version" do
       response.pim.version.should eq "14.0.7113"
-      response.pim_version.should eq "14.0.7113"
     end
   end
 
@@ -218,11 +192,9 @@ describe "Response" do
     it "should return true if PIM detected" { response.portable_media_player?.should be_true }
     it "should return vendor" do
       response.portable_media_player.vendor.should eq "Microsoft"
-      response.portable_media_player_vendor.should eq "Microsoft"
     end
     it "should return model" do
       response.portable_media_player.model.should eq "Zune HD"
-      response.portable_media_player_model.should eq "Zune HD"
     end
   end
 
@@ -233,11 +205,9 @@ describe "Response" do
     it "should return true if TV detected" { response.tv?.should be_true }
     it "should return vendor" do
       response.tv.vendor.should eq "Hisense"
-      response.tv_vendor.should eq "Hisense"
     end
     it "should return model" do
       response.tv.model.should eq "HE65M7000UWTS"
-      response.tv_model.should eq "HE65M7000UWTS"
     end
   end
 
@@ -248,7 +218,6 @@ describe "Response" do
     it "should return true if VendorFragment detected" { response.vendorfragment?.should be_true }
     it "should return vendor" do
       response.vendorfragment.vendor.should eq "Acer"
-      response.vendorfragment_vendor.should eq "Acer"
     end
   end
 
