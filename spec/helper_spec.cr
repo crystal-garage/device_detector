1aa9a6cc3d811dcc4a6b402c498498d41f33d33e
Anton Maminov <anton.linux@gmail.com>
2025-08-01 12:42:39 +0300

    Update helper_spec.cr

--- spec/helper_spec.cr
+++ spec/helper_spec.cr
@@ -4,9 +4,4 @@ describe "Helper" do
   it "should detect capture group" do
     DeviceDetector::Helper.capture_groups?("$1").should be_true
   end
-
-  it "should fill capture groups" do
-    # FIXME
-    true.should be_true
-  end
 end
